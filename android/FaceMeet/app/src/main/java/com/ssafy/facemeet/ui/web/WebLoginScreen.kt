833bf8ee85411781548b773b0a71115e8d49c542
yunsungjun <yun9999999999yun@gmail.com>
2025-07-31 19:53:42 +0900

    fix : merge로 인한 사라진 webViewLogin BackBtn 추가

--- android/FaceMeet/app/src/main/java/com/ssafy/facemeet/ui/web/WebLoginScreen.kt
+++ android/FaceMeet/app/src/main/java/com/ssafy/facemeet/ui/web/WebLoginScreen.kt
@@ -3,15 +3,28 @@ package com.ssafy.facemeet.ui.web
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 
@@ -37,7 +50,31 @@ fun WebLoginScreen(
     }
 
     Scaffold(
-        containerColor = Color.White
+        containerColor = Color.White,
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(70.dp), // topBar 전체 높이
+                contentAlignment = Alignment.CenterStart
+            ) {
+                Box(
+                    modifier = Modifier
+                        .padding(start = 10.dp)
+                        .size(width = 50.dp, height = 70.dp)
+                        .clip(RoundedCornerShape(8.dp)) // 네모형 (둥글기 살짝)
+                        .background(Color.White)
+                        .clickable { onCancel() },
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = "뒤로가기",
+                        tint = Color.Black
+                    )
+                }
+            }
+        }
     ) { paddingValues ->
         AndroidView(
             factory = {
