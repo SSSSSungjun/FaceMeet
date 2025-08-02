3040e89025f528cecda3e456b77277e1a7b8996a
cheonjiyun <gunbam715@gmail.com>
2025-08-03 00:54:22 +0900

    ui : 매칭로딩중 화면

--- android/FaceMeet/client/src/main/java/com/ssafy/facemeet/client/navigation/client/ClientNavHost.kt
+++ android/FaceMeet/client/src/main/java/com/ssafy/facemeet/client/navigation/client/ClientNavHost.kt
@@ -39,7 +39,7 @@ fun NavGraphBuilder.clientNavHost(navController: NavHostController) {
     }
 
     composable(ClientRoutes.MatchingLoading.route) {
-        MatchingLoadingScreen()
+        MatchingLoadingScreen(onCancel = { navController.popBackStack() })
     }
 
     composable(
