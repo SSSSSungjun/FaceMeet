1e25379cf6c74a84b58f25d55d3bf5588d59c910
DESKTOP-RBNO6LS\SSAFY <gunbam715@gmail.com>
2025-08-08 17:29:15 +0900

    fix: 매칭권 사용시 앱이 종료되는 오류 임시 수정

--- android/FaceMeet/client/src/main/java/com/ssafy/facemeet/client/navigation/client/ClientNavHost.kt
+++ android/FaceMeet/client/src/main/java/com/ssafy/facemeet/client/navigation/client/ClientNavHost.kt
@@ -52,13 +52,14 @@ fun NavGraphBuilder.clientNavHost(
             onCancel = { navController.popBackStack() },
             onMatchFound = { matchedChatRoomId ->
 
-                // 2. 채팅리스트 밑에 깔고,
-                bottomNavController.navigate(BottomNavRoutes.ChattingList.route) {
-                    popUpTo(BottomNavRoutes.Matching.route) { inclusive = true }
-                    launchSingleTop = true
-                }
+//                // 1. 채팅리스트 밑에 깔고 -> 오류원인을 찾기 전까지 주석처리해놓을게요
+                //Navigation graph has not been set for NavController androidx.navigation.internal.NavControllerImpl
+//                bottomNavController.navigate(BottomNavRoutes.ChattingList.route) {
+//                    popUpTo(BottomNavRoutes.Matching.route) { inclusive = true }
+//                    launchSingleTop = true
+//                }
 
-                // 3. 그 다음 Chat 진입
+                // 2. 그 다음 Chat 진입
                 navController.navigate(ClientRoutes.Chat.createRoute(matchedChatRoomId)) {
                     popUpTo(ClientRoutes.MatchingLoading.route) { inclusive = true } // 로딩까지 제거
                     launchSingleTop = true
@@ -84,7 +85,7 @@ fun NavGraphBuilder.clientNavHost(
         ChattingScreen(
             roomId = roomId,
             onBackClick = {
-               // navController.popBackStack()
+                // navController.popBackStack()
                 navController.navigate(BottomNavRoutes.ChattingList.route) {
                     popUpTo(ClientRoutes.Chat.route) {
                         inclusive = true
