537f2852a7171cc28cd700fe4054833574337041
yunsungjun <yun9999999999yun@gmail.com>
2025-08-17 20:08:20 +0900

    글자색 바꿈

--- android/FaceMeet/client/src/main/java/com/ssafy/facemeet/client/ui/chatlist/ChattingListScreen.kt
+++ android/FaceMeet/client/src/main/java/com/ssafy/facemeet/client/ui/chatlist/ChattingListScreen.kt
@@ -56,6 +56,7 @@ import com.ssafy.facemeet.client.R
 import com.ssafy.facemeet.client.ui.profile.partner.dialog.RoomExitDialog
 import com.ssafy.facemeet.client.ui.theme.ChosunCentennial
 import com.ssafy.facemeet.core.domain.model.ChatListItem
+import com.ssafy.facemeet.core.util.constant.CommonColor
 import kotlin.math.abs
 
 private const val TAG = "ChattingListScreen"
@@ -82,7 +83,8 @@ fun ChattingListScreen(
                 .padding(15.dp),
             text = "채팅 목록",
             fontSize = 18.sp,
-            fontFamily = ChosunCentennial
+            fontFamily = ChosunCentennial,
+            color= CommonColor.Brown500
         )
         NoticeBanner()
 
